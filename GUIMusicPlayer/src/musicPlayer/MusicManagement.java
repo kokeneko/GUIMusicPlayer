e671a6afa8037ae2ef75d3f6afe843add25673d6
kokeneko <s17t278@stu.kagawa-u.ac.jp>
2018-05-14 00:23:26 +0900

    音楽を再生できるようにした

--- GUIMusicPlayer/src/musicPlayer/MusicManagement.java
+++ GUIMusicPlayer/src/musicPlayer/MusicManagement.java
@@ -1,10 +1,43 @@
 package musicPlayer;
 
+import java.io.File;
+
+import javazoom.jlgui.basicplayer.BasicPlayer;
+import javazoom.jlgui.basicplayer.BasicPlayerException;
+
 public class MusicManagement {
 
-	//曲名を受け取ってコンソールに表示
-	public void playMusic(String name) {
+	BasicPlayer player;
+
+	//曲名を受け取ってインスタンスを生成
+	public MusicManagement(String name) throws BasicPlayerException {
 		System.out.println(name);
+
+		player = new BasicPlayer();
+		File file = new File(".\\src\\Music\\" + name);
+
+		player.open(file);
+
+		player.play();
 	}
+
+	//再生
+	public void playMusic() throws BasicPlayerException {
+		player.play();
+	}
+	//一時停止
+	private void pauseMusic() throws BasicPlayerException {
+		player.pause();
+	}
+	//再開
+	private void resumeMusic() throws BasicPlayerException {
+		player.resume();
+	}
+	//停止
+	private void stopMusic() throws BasicPlayerException {
+		player.resume();
+	}
+
+
 
 }
--- GUIMusicPlayer/src/musicPlayer/PlayerController.java
+++ GUIMusicPlayer/src/musicPlayer/PlayerController.java
@@ -3,10 +3,11 @@ package musicPlayer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javazoom.jlgui.basicplayer.BasicPlayerException;
 
 public class PlayerController {
 
-	@FXML private Button playButton;
+	@FXML private Button playButton, pauseButton, resumeButton, stopButton;
 	@FXML private ListView<String> musicList;
 
 	@FXML
@@ -21,11 +22,26 @@ public class PlayerController {
 
 
 	@FXML
-	private void playMusic() {
+	private void playMusic() throws BasicPlayerException {
 		String musicName = musicList.getSelectionModel().getSelectedItem();
 
-		MusicManagement music = new MusicManagement();
+		MusicManagement music = new MusicManagement(musicName);
 
-		music.playMusic(musicName);
+		music.playMusic();
+	}
+
+	@FXML
+	private void pauseMusic() throws BasicPlayerException {
+
+	}
+
+	@FXML
+	private void resumeMusic() throws BasicPlayerException {
+
+	}
+
+	@FXML
+	private void stopMusic() throws BasicPlayerException {
+
 	}
 }
