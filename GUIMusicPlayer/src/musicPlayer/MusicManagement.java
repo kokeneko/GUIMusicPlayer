ee20a077d6cb151366de1ddf450d94c1fb8a7092
kokeneko <s17t278@stu.kagawa-u.ac.jp>
2018-05-15 00:33:03 +0900

    一時停止の追加と、重複して再生できないようにした

--- GUIMusicPlayer/src/musicPlayer/MusicManagement.java
+++ GUIMusicPlayer/src/musicPlayer/MusicManagement.java
@@ -7,7 +7,8 @@ import javazoom.jlgui.basicplayer.BasicPlayerException;
 
 public class MusicManagement {
 
-	BasicPlayer player;
+	public static BasicPlayer player;
+	public int musicStatus = 2; //STOPPED
 
 	//曲名を受け取ってインスタンスを生成
 	public MusicManagement(String name) throws BasicPlayerException {
@@ -17,25 +18,37 @@ public class MusicManagement {
 		File file = new File(".\\src\\Music\\" + name);
 
 		player.open(file);
+		musicStatus = BasicPlayer.OPENED;
+		System.out.println(musicStatus);
+	}
 
-		player.play();
+	public MusicManagement() throws BasicPlayerException {
+
 	}
 
 	//再生
 	public void playMusic() throws BasicPlayerException {
 		player.play();
+		musicStatus = BasicPlayer.PLAYING;
+		System.out.println(musicStatus);
 	}
 	//一時停止
-	private void pauseMusic() throws BasicPlayerException {
+	public void pauseMusic() throws BasicPlayerException {
 		player.pause();
+		musicStatus = BasicPlayer.PAUSED;
+		System.out.println(musicStatus);
 	}
 	//再開
-	private void resumeMusic() throws BasicPlayerException {
+	public void resumeMusic() throws BasicPlayerException {
 		player.resume();
+		musicStatus = BasicPlayer.PLAYING;
+		System.out.println(musicStatus);
 	}
 	//停止
-	private void stopMusic() throws BasicPlayerException {
-		player.resume();
+	public void stopMusic() throws BasicPlayerException {
+		player.stop();
+		musicStatus = BasicPlayer.STOPPED;
+		System.out.println(musicStatus);
 	}
 
 
--- GUIMusicPlayer/src/musicPlayer/PlayerController.java
+++ GUIMusicPlayer/src/musicPlayer/PlayerController.java
@@ -3,6 +3,7 @@ package musicPlayer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javazoom.jlgui.basicplayer.BasicPlayer;
 import javazoom.jlgui.basicplayer.BasicPlayerException;
 
 public class PlayerController {
@@ -10,38 +11,47 @@ public class PlayerController {
 	@FXML private Button playButton, pauseButton, resumeButton, stopButton;
 	@FXML private ListView<String> musicList;
 
+	public static MusicManagement music;
+
 	@FXML
-	private void initialize() {
+	private void initialize() throws BasicPlayerException {
 		FileManagement filemanage = new FileManagement();
 		int number = filemanage.musicNumber();
 
 		for (int i = 0; i < number; i++) {
 			musicList.getItems().add(filemanage.musicName(i));
 		}
+
+		music = new MusicManagement();
 	}
 
 
 	@FXML
 	private void playMusic() throws BasicPlayerException {
+
+		if (music.musicStatus == BasicPlayer.PLAYING) {
+			music.stopMusic();
+		}
+
 		String musicName = musicList.getSelectionModel().getSelectedItem();
 
-		MusicManagement music = new MusicManagement(musicName);
+		music = new MusicManagement(musicName);
 
 		music.playMusic();
 	}
 
 	@FXML
 	private void pauseMusic() throws BasicPlayerException {
-
+		music.pauseMusic();
 	}
 
 	@FXML
 	private void resumeMusic() throws BasicPlayerException {
-
+		music.resumeMusic();
 	}
 
 	@FXML
 	private void stopMusic() throws BasicPlayerException {
-
+		music.stopMusic();
 	}
 }
